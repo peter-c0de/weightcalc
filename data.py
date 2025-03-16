00003686669eb2674cf3936967f76796a7829a49
Peter c0de <pete_coder@hotmail.com>
2025-03-16 16:00:45 +0200

    File: data.py Desc: Edit this file every week with the new data.

---
+++ data.py
@@ -0,0 +1,91 @@
+class Data:
+    # Main Data:
+    starting_date = "5 January 2025"
+    starting_weight = "100 KGs"
+    first_month = "95 KGs"
+    second_month = "90 KGs"
+    third_month = "March"
+    fourth_month = "April"
+    fifth_month = "May"
+    
+    # Data Changes Every Week:
+    this_week_number = 11
+    starting_current_week = 85
+    
+    # Weeks' Data:
+    
+    # 1st Month:
+    week1_loss = "1.25" 
+    week1_kgs = "98.75 KGs"
+    week2_loss = "1.25"
+    week2_kgs = "97.50 KGs"
+    week3_loss = "1.25" 
+    week3_kgs = "96.25 KGs"
+    week4_loss = "1.25" 
+    week4_kgs = "95.00 KGs"
+    
+    # 2nd Month:
+    week5_loss = "1.25" 
+    week5_kgs = "93.75 KGs"
+    week6_loss = "1.25" 
+    week6_kgs = "92.50 KGs"
+    week7_loss = "1.25" 
+    week7_kgs = "91.25 KGs"
+    week8_loss = "1.25" 
+    week8_kgs = "90.00 KGs"
+
+    # 3rd Month:
+    week9_loss = "1.25" 
+    week9_kgs = "88.75 KGs"
+    week10_loss = "1.25" 
+    week10_kgs = "87.50 KGs"
+    week11_loss = "0" 
+    week11_kgs = "0 KGs"
+    week12_loss = "0" 
+    week12_kgs = "0 KGs"
+    
+    # 4th Month:
+    week13_loss = "0" 
+    week13_kgs = "0 KGs"
+    week14_loss = "0" 
+    week14_kgs = "0 KGs"
+    week15_loss = "0" 
+    week15_kgs = "0 KGs"
+    week16_loss = "0" 
+    week16_kgs = "0 KGs"
+    
+    # 5th Month:
+    week17_loss = "0" 
+    week17_kgs = "0 KGs"
+    week18_loss = "0" 
+    week18_kgs = "0 KGs"
+    week19_loss = "0" 
+    week19_kgs = "0 KGs"
+    week20_loss = "0" 
+    week20_kgs = "0 KGs"
+    
+    # 6th Month:
+    week21_loss = "0" 
+    week21_kgs = "0 KGs"
+    week22_loss = "0" 
+    week22_kgs = "0 KGs"
+    week23_loss = "0" 
+    week23_kgs = "0 KGs"
+    week24_loss = "0" 
+    week24_kgs = "0 KGs"
+    
+    # 7th Month
+    week25_loss = "0" 
+    week25_kgs = "0 KGs"
+    week26_loss = "0" 
+    week26_kgs = "0 KGs"
+    week27_loss = "0" 
+    week27_kgs = "0 KGs"
+    week28_loss = "0" 
+    week28_kgs = "0 KGs"
+
+    # Please Let the 7th Month be the Final Month
+    week29_loss = "0" 
+    week29_kgs = "0 KGs"
+    week30_loss = "0" 
+    week30_kgs = "0 KGs"
